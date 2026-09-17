3d1a9cf292fbd9668e2388e6db99b49f6eb4fcf1
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-17 19:29:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x3cb2534E7CE0d94182c2B58c775b74C9a0f265f8",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/wireoffice_rh",
   NAME:    "Wire Office",
   TICKER:  "$WIRE",
